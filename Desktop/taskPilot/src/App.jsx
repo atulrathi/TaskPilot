6d3926c4ab3dc86f8efdaa86628bceb9b5376fed
atul rathi <rathiatul43@gmail.com>
2025-07-07 21:25:38 +0530

    complete

--- Desktop/taskPilot/src/App.jsx
+++ Desktop/taskPilot/src/App.jsx
@@ -3,47 +3,48 @@ import Login from './components/auth/Login';
 import Employed from './components/Dashbord/Employedashbord';
 import Admin from './components/Dashbord/AdminDashbord';
 import { Authcontext } from './context/Authprovider';
+import "./App.css";
 
 function App() {
-   const [user ,setuser]=useState(null)
-   const [loggedInuserdata,setloggedInuserdata]=useState(null)
-   const [userdata,setuserdata]=useContext(Authcontext)
+  const [user, setuser] = useState(null)
+  const [loggedInuserdata, setloggedInuserdata] = useState(null)
+  const [userdata, setuserdata] = useContext(Authcontext)
 
-   useEffect(()=>{
-   const loggedInUser=localStorage.getItem('loggedInUser');
+  useEffect(() => {
+    const loggedInUser = localStorage.getItem('loggedInUser');
 
-    if(loggedInUser){
-      const usedata=JSON.parse(loggedInUser);
+    if (loggedInUser) {
+      const usedata = JSON.parse(loggedInUser);
       setuser(usedata.role);
       setloggedInuserdata(usedata.data)
     }
-   },[])
+  }, [])
 
-   const handlogin=(email,password)=>{
-    if(email=="admin@example.com" && password=='123'){
+  const handlogin = (email, password) => {
+    if (email == "admin@example.com" && password == '123') {
       setuser('admin')
-      localStorage.setItem('loggedInUser',JSON.stringify({role:'admin'}))
-    }else if(userdata ){
-      const employee=userdata.find((e)=>email==e.email && e.password==password)
-      if(employee){
+      localStorage.setItem('loggedInUser', JSON.stringify({ role: 'admin' }))
+    } else if (userdata) {
+      const employee = userdata.find((e) => email == e.email && e.password == password)
+      if (employee) {
         setuser('employees')
         setloggedInuserdata(employee)
-        localStorage.setItem('loggedInUser',JSON.stringify({role:'employees',data:employee}))
+        localStorage.setItem('loggedInUser', JSON.stringify({ role: 'employees', data: employee }))
       }
-    }else{
-    alert("invalid credentials")
-     
+    } else {
+      alert("invalid credentials")
+
     }
-   }
+  }
 
 
   return (
     <>
-    {!user?<Login  handlogin={handlogin}/>:''}
-   { user=='admin'?<Admin data={loggedInuserdata} changeuser={setuser}/>: ( user=='employees' ? <Employed data={loggedInuserdata} changeuser={setuser} /> : null) }
-  
+      {!user ? <Login handlogin={handlogin} /> : ''}
+      {user == 'admin' ? <Admin data={loggedInuserdata} changeuser={setuser} /> : (user == 'employees' ? <Employed data={loggedInuserdata} changeuser={setuser} /> : null)}
+
     </>
-    );
+  );
 }
 
 export default App;
--- Desktop/taskPilot/src/components/auth/Login.jsx
+++ Desktop/taskPilot/src/components/auth/Login.jsx
@@ -1,39 +1,40 @@
-import React, {useState } from 'react';
+import React, { useState } from 'react';
 
-function Login({handlogin}) {
+function Login({ handlogin }) {
 
-  const [email,setemail]=useState('')  
-  const [password,setpass]=useState('')  
+  const [email, setemail] = useState('')
+  const [password, setpass] = useState('')
 
-  const submitHandler=(e)=>{
+  const submitHandler = (e) => {
     e.preventDefault()
-
-    console.log("email is ",email)
-    console.log("password is",password);
-    handlogin(email,password)
+    handlogin(email, password)
     setemail("")
     setpass("")
   }
-  return ( 
-   <div id='logn' className='flex  h-screen w-screen items-center justify-center bg-black'>
-    <div className='border-2 border-emerald-600 py-50 px-50'>
-      <form 
-      onSubmit={(e)=>
-      {submitHandler(e)}} className='flex flex-col items-center justify-center'>
-        <input value={email}
-        onChange={(e)=>{
-          setemail(e.target.value)
-          }} required className='border-4 border-emerald-600 py-3 px-3 text-xl text-white outline-none bg-transparent rounded-full  placeholder:text-white' type="email" placeholder='enter your email'/>
-        <input value={password}
-         onChange={(e)=>{
-          setpass(e.target.value)
-         }}
-         required className='border-4 border-emerald-600 py-3 px-3 text-xl text-white outline-none bg-transparent rounded-full mt-15 placeholder:text-white'  type="password" placeholder='password'/>
-        <button className='border-2 mt-20 bg-emerald-600 py-3 psx-3 text-xl outline-none w-60 rounded-full'>Log in</button>
-      </form>
+  return (
+    <div className='logn '>
+      <div className="anda1"></div>
+      <div className="anda2"></div>
+      <div className="anda3"></div>
+      <div className="anda4"></div>
+      <div className='logn2'>
+        <p>Log in </p>
+        <form
+          onSubmit={(e) => { submitHandler(e) }} >
+          <input value={email}
+            onChange={(e) => {
+              setemail(e.target.value)
+            }} required className='input' type="email" placeholder='enter your email' />
+          <input value={password}
+            onChange={(e) => {
+              setpass(e.target.value)
+            }}
+            required className='input2' type="password" placeholder='password' />
+          <button className='loginbtn'>Log in</button>
+        </form>
+      </div>
     </div>
-   </div>
-   );
+  );
 }
 
 export default Login;
